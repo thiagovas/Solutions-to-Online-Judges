1562abd3c641646382d20b8c259ead033e04255a
Thiago Vieira <thiagovieiraas@gmail.com>
2017-03-05 16:57:13 -0300

    Changed the template

--- template.cc
+++ template.cc
@@ -2,16 +2,20 @@
 using namespace std;
 
 #define endl "\n"
-#define PII pair<int, int>
-#define PLL pair<ll, ll>
+#define EPS 1e-7
+#define INF 0x3f3f3f3f
+#define LINF 0x3f3f3f3f3f3f3f3fll
+#define mp make_pair
+#define pb push_back
 typedef long long int ll;
 typedef long double ld;
-
+typedef pair<int, int> ii;
+typedef vector<int> vi;
 
 int main()
 {
   ios::sync_with_stdio(false);  cin.tie(0);  cout.tie(0);
-  
-  
+
+
   return 0;
 }
