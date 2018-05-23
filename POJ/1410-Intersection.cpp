89e9e9c0404d587fc3f4cc7f4fb766efad048248
Thiago Silva <thiagovieiraas@gmail.com>
2018-05-23 13:49:11 -0300

    Added one solution for the 1410 POJ.

---
+++ POJ/1410-Intersection.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+
+double INF = 1e100;
+double EPS = 1e-12;
+
+struct PT { 
+  double x, y; 
+  PT() {}
+  PT(double x, double y) : x(x), y(y) {}
+  PT(const PT &p) : x(p.x), y(p.y)    {}
+  PT operator + (const PT &p)  const { return PT(x+p.x, y+p.y); }
+  PT operator - (const PT &p)  const { return PT(x-p.x, y-p.y); }
+  PT operator * (double c)     const { return PT(x*c,   y*c  ); }
+  PT operator / (double c)     const { return PT(x/c,   y/c  ); }
+};
+
+double dot(PT p, PT q)     { return p.x*q.x+p.y*q.y; }
+double dist2(PT p, PT q)   { return dot(p-q,p-q); }
+double cross(PT p, PT q)   { return p.x*q.y-p.y*q.x; }
+
+// project point c onto line through a and b
+// assuming a != b
+PT ProjectPointLine(PT a, PT b, PT c) {
+  return a + (b-a)*dot(c-a, b-a)/dot(b-a, b-a);
+}
+
+// project point c onto line segment through a and b
+PT ProjectPointSegment(PT a, PT b, PT c) {
+  double r = dot(b-a,b-a);
+  if (fabs(r) < EPS) return a;
+  r = dot(c-a, b-a)/r;
+  if (r < 0) return a;
+  if (r > 1) return b;
+  return a + (b-a)*r;
+}
+
+// compute distance from c to segment between a and b
+double DistancePointSegment(PT a, PT b, PT c) {
+  return sqrt(dist2(c, ProjectPointSegment(a, b, c)));
+}
+
+// determine if lines from a to b and c to d are parallel or collinear
+bool LinesParallel(PT a, PT b, PT c, PT d) { 
+  return fabs(cross(b-a, c-d)) < EPS; 
+}
+
+bool LinesCollinear(PT a, PT b, PT c, PT d) { 
+  return LinesParallel(a, b, c, d)
+    && fabs(cross(a-b, a-c)) < EPS
+    && fabs(cross(c-d, c-a)) < EPS; 
+}
+
+// determine if line segment from a to b intersects with 
+// line segment from c to d
+bool SegmentsIntersect(PT a, PT b, PT c, PT d) {
+  if (LinesCollinear(a, b, c, d)) {
+    if (dist2(a, c) < EPS || dist2(a, d) < EPS ||
+        dist2(b, c) < EPS || dist2(b, d) < EPS) return true;
+    if (dot(c-a, c-b) > 0 && dot(d-a, d-b) > 0 && dot(c-b, d-b) > 0)
+      return false;
+    return true;
+  }
+  if (cross(d-a, b-a) * cross(c-a, b-a) > 0) return false;
+  if (cross(a-c, d-c) * cross(b-c, d-c) > 0) return false;
+  return true;
+}
+
+// determine if point is in a possibly non-convex polygon (by William
+// Randolph Franklin); returns 1 for strictly interior points, 0 for
+// strictly exterior points, and 0 or 1 for the remaining points.
+// Note that it is possible to convert this into an *exact* test using
+// integer arithmetic by taking care of the division appropriately
+// (making sure to deal with signs properly) and then by writing exact
+// tests for checking point on polygon boundary
+bool PointInPolygon(const vector<PT> &p, PT q) {
+  bool c = 0;
+  for (int i = 0; i < p.size(); i++){
+    int j = (i+1)%p.size();
+    if ((p[i].y <= q.y && q.y < p[j].y || 
+          p[j].y <= q.y && q.y < p[i].y) &&
+        q.x < p[i].x + (p[j].x - p[i].x) * (q.y - p[i].y) / (p[j].y - p[i].y))
+      c = !c;
+  }
+  return c;
+}
+
+int main() {
+  int t;
+  cin >> t;
+
+  while(t--) {
+    vector<PT> rectangle;
+    PT psline, peline;
+    PT plefttop, prightbottom;
+
+    cin >> psline.x >> psline.y;
+    cin >> peline.x >> peline.y;
+    cin >> plefttop.x >> plefttop.y;
+    cin >> prightbottom.x >> prightbottom.y;
+
+
+    rectangle.pb(plefttop);
+    rectangle.pb(PT(prightbottom.x, plefttop.y));
+    rectangle.pb(prightbottom);
+    rectangle.pb(PT(plefttop.x, prightbottom.y));
+
+    if(PointInPolygon(rectangle, psline) or PointInPolygon(rectangle, peline))
+      cout << "T\n";
+    else {
+      bool T = false;
+      for(int i = 0; i < 4; i++) {
+        if(SegmentsIntersect(rectangle[i], rectangle[(i+1)%4], psline, peline)) {
+          T = true;
+          break;
+        }
+      }
+      if(T) cout << "T\n";
+      else cout << "F\n";
+    }
+  }
+  return 0;
+}
+
