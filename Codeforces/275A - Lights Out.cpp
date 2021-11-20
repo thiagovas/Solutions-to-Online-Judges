563e484219e1b52ac814670320c779e5b63535a6
vijaykumar360 <75715269+vijaykumar360@users.noreply.github.com>
2021-11-20 19:18:02 +0530

    Update 275A - Lights Out.cpp
    this code is simple solution than the before one  and works very well.
    i hope you accept it.

--- Codeforces/275A - Lights Out.cpp
+++ Codeforces/275A - Lights Out.cpp
@@ -1,104 +1,29 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int tab[3][3];
-    bool result[3][3];
-    int i, j;
-    
-    for(i = 0; i < 3; i++)
-        for(j = 0; j < 3; j++)
-            result[i][j] = true;
-    
-    for(i = 0; i < 3; i++)
+    #include<iostream>
+    using namespace std;
+    int main()
     {
-        for(j = 0; j < 3; j++)
-            cin >> tab[i][j];
-    }
-    
-    if(tab[0][0]%2 != 0)
-    {
-        result[0][0] = !result[0][0];
-        result[1][0] = !result[1][0];
-        //result[1][1] = !result[1][1];
-        result[0][1] = !result[0][1];
-    }
-    if(tab[1][0]%2 != 0)
-    {
-        result[0][0] = !result[0][0];
-        result[1][0] = !result[1][0];
-        result[2][0] = !result[2][0];
-        //result[0][1] = !result[0][1];
-        result[1][1] = !result[1][1];
-        //result[2][1] = !result[2][1];
-    }
-    if(tab[2][0]%2 != 0)
-    {
-        //result[1][1] = !result[1][1];
-        result[2][0] = !result[2][0];
-        result[2][1] = !result[2][1];
-        result[1][0] = !result[1][0];
-    }
-    if(tab[0][1]%2 != 0)
-    {
-        result[0][0] = !result[0][0];
-        //result[1][0] = !result[1][0];
-        result[0][1] = !result[0][1];
-        result[1][1] = !result[1][1];
-        //result[1][2] = !result[1][2];
-        result[0][2] = !result[0][2];
-    }
-    if(tab[1][1]%2 != 0)
-    {
-        result[1][1] = !result[1][1];
-        result[0][1] = !result[0][1];
-        result[2][1] = !result[2][1];
-        result[1][0] = !result[1][0];
-        result[1][2] = !result[1][2];
-    }
-    if(tab[2][1]%2 != 0)
-    {
-        //result[1][0] = !result[1][0];
-        result[1][1] = !result[1][1];
-        //result[1][2] = !result[1][2];
-        result[2][0] = !result[2][0];
-        result[2][1] = !result[2][1];
-        result[2][2] = !result[2][2];
-    }
-    if(tab[0][2]%2 != 0)
-    {
-        //result[1][1] = !result[1][1];
-        result[0][1] = !result[0][1];
-        result[0][2] = !result[0][2];
-        result[1][2] = !result[1][2];
-    }
-    if(tab[1][2]%2 != 0)
-    {
-        //result[0][1] = !result[0][1];
-        result[0][2] = !result[0][2];
-        result[1][1] = !result[1][1];
-        result[1][2] = !result[1][2];
-        //result[2][1] = !result[2][1];
-        result[2][2] = !result[2][2];
-    }
-    if(tab[2][2]%2 != 0)
-    {
-        result[2][2] = !result[2][2];
-        result[2][1] = !result[2][1];
-        result[1][2] = !result[1][2];
-        //result[1][1] = !result[1][1];
-    }
-    
-    for(i = 0; i < 3; i++)
-    {
-        for(j = 0; j < 3; j++)
+        int arr[3][3];
+        bool a[3][3]={1,1,1,1,1,1,1,1,1};
+        for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++)
+            cin>>arr[i][j];
+            for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++)
+            {
+                if((arr[i][j]+((i-1<0)?0:arr[i-1][j])+((i+1>2)?0:arr[i+1][j])+((j-1<0)?0:arr[i][j-1])+((j+1>2)?0:arr[i][j+1]))&1)
+                    a[i][j]=0;
+            }
+        for(int i=0;i<3;i++)
         {
-            if(result[i][j]) cout << 1;
-            else cout << 0;
+
+
+            for(int j=0;j<3;j++)
+
+
+
+            cout<<a[i][j];
+
+
+            cout<<"\n";
         }
-        cout << '\n';
-    }        
-    
-    return 0;
-}
+    }
